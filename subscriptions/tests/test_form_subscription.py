813d478039fa30d657c25cff53070dc670f3d370
kelver <kelverwt@gmail.com>
2022-12-02 13:08:21 -0300

    Valida CPF

--- subscriptions/tests/test_form_subscription.py
+++ subscriptions/tests/test_form_subscription.py
@@ -10,3 +10,21 @@ class SubscriptionFormTest(TestCase):
         """Form must have 4 fields"""
         expected = ['name', 'cpf', 'email', 'phone']
         self.assertSequenceEqual(expected, list(self.form.fields))
+
+    def test_cpf_is_digit(self):
+        """CPF must only acept digits"""
+        data = dict(name='Kelver Alves', cpf='ABCD5678901',
+                    email='kelverwt@gmail.com', phone='84996068403')
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+        self.assertListEqual(['cpf'], list(form.errors))
+
+    def test_cpf_has_11_digits(self):
+        """CPF must have 11 digits"""
+        data = dict(name='Kelver Alves', cpf='1234',
+                    email='kelverwt@gmail.com', phone='84996068403')
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+        self.assertListEqual(['cpf'], list(form.errors))
