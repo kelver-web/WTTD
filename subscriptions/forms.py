624a39245860f50f13d71966af006ac0dfc43cc9
kelver <kelverwt@gmail.com>
2022-12-02 19:15:27 -0300

    Capitalizando o name

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -4,12 +4,21 @@ from django.core.exceptions import ValidationError
 
 def validate_cpf(value):
     if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números.')
+        raise ValidationError('CPF deve conter apenas números.', 'digits')
     if len(value) != 11:
-        raise ValidationError('CPF deve conter 11 números.')
+        raise ValidationError('CPF deve conter 11 números.', 'length')
 
 class SubscriptionForm(forms.Form):
     name  = forms.CharField(label='Nome')
     cpf   = forms.CharField(label='Cpf', validators=[validate_cpf])
     email = forms.EmailField(label='Email')
     phone = forms.CharField(label='Telefone')
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        # words = []
+        # for w in name.split():
+        #     words.append(w.capitalize())
+        words = [w.capitalize() for w in name.split()]
+
+        return ' '.join(words)
--- subscriptions/tests/test_form_subscription.py
+++ subscriptions/tests/test_form_subscription.py
@@ -13,18 +13,34 @@ class SubscriptionFormTest(TestCase):
 
     def test_cpf_is_digit(self):
         """CPF must only acept digits"""
-        data = dict(name='Kelver Alves', cpf='ABCD5678901',
-                    email='kelverwt@gmail.com', phone='84996068403')
-        form = SubscriptionForm(data)
-        form.is_valid()
-
-        self.assertListEqual(['cpf'], list(form.errors))
-
+        form = self.make_validated_form(cpf='ABCD5678901')
+        self.assertFormErrorCode(form, 'cpf', 'digits')
+       
     def test_cpf_has_11_digits(self):
         """CPF must have 11 digits"""
-        data = dict(name='Kelver Alves', cpf='1234',
+        form = self.make_validated_form(cpf='1234')
+        self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def test_name_must_be_capitalized(self):
+        """Name must be capitalized"""
+        form = self.make_validated_form(name='KELVER alves')
+        self.assertEqual('Kelver Alves', form.cleaned_data['name'])
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    def assertFormErrorMessage(self, form, field, msg):
+        errors = form.errors
+        errors_list = errors[field]
+        self.assertListEqual([msg], errors_list)
+        
+    def make_validated_form(self, **kwargs):
+        valid = dict(name='Kelver Alves', cpf='12345678901',
                     email='kelverwt@gmail.com', phone='84996068403')
+        data = dict(valid, **kwargs)
         form = SubscriptionForm(data)
         form.is_valid()
-
-        self.assertListEqual(['cpf'], list(form.errors))
+        return form
