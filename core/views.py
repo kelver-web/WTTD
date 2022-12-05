697880aeeca853096bc31f51a6830b7f0d8bbb43
kelver <kelverwt@gmail.com>
2022-12-05 10:56:29 -0300

    Keynot Speakers

--- core/views.py
+++ core/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    speakers = [
+        { 'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},    
+        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
+    ]
+    return render(request, 'index.html', {'speakers': speakers})
