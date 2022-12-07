a4ad41269dd4db3433410cb0a049f476d14c7c87
kelver <kelverwt@gmail.com>
2022-12-07 15:34:02 -0300

    Contact

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import Speaker
+from .models import Speaker, Contact
 
 # Register your models here.
 
+
+class ContactInLine(admin.TabularInline):
+    model = Contact
+    extra = 1
+
 class SpeakerAdmin(admin.ModelAdmin):
+    inlines = [ContactInLine]
     prepopulated_fields = {'slug': ('name',)}
     list_display = ['name', 'photo_img', 'website_link']
 
